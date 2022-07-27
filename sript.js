6725f841d2ee3deb7cda15daeabf1850114138fa
faiFKAFAKLJFAEOIKLFEOKJkjngok <110091628+faiFKAFAKLJFAEOIKLFEOKJkjngok@users.noreply.github.com>
2022-07-27 11:36:24 +0300

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,12 @@
+var b1 = document.getElementById("b1")
+var h11 = document.getElementById("h11")
+
+b1.onclick = function() {
+    h11.style.color = "red";
+}
+
+var b2 = document.getElementById("b2")
+
+b2.onclick = function() {
+    h11.style.color = "green";
+}
